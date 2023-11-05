38b8dbba8be34f4e1c40f9420fef4ed9bf71a39d
Vladislav <vladislavkukochka@gmail.com>
2023-11-05 11:16:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter basename="/goit-react-hw-01-components">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
